018b76c2185881ade4a8e0390c4029ae7c8f636a
andkoc001 <and.koc001@gmail.com>
2019-03-31 21:20:43 +0100

    iris.py created with some meta information in the header section.

---
+++ iris.py
@@ -0,0 +1,11 @@
+# Title: Fisher's Iris Data Set Analysis
+# Description: Program is a playground for data analysis, as described in Readme.md file on Git Hub repository.
+# Context: Programming and Scripting, GMIT, 2019
+# Author: Andrzej Kocielski
+# Email: G00376291@gmit.ie
+# Date of creation: 31-03-2019
+# Last update: 31-03-2019
+
+###
+
+# Importing libraries
