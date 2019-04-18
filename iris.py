1138df8fdb3c9f56e40b109d284d85be22aaab08
andkoc001 <and.koc001@gmail.com>
2019-04-18 08:10:25 +0100

    Data set divided into two sub sets - with numerical values (columns 1-4) and with names (column 5).

--- iris.py
+++ iris.py
@@ -15,14 +15,14 @@ import matplotlib.pyplot as pl
 
 # Rading the csv data file, assigning variable to its content
 # Attribute header=None assumes that there is no header line in the raw csv file - the first row is actual data
-ids = pd.read_csv("iris_dataset.csv", header=None)
+ids = pd.read_csv("iris_dataset.csv", header=None)  # ids - iris data set
 
 # Adding headers to attributes (columns); source: https://stackoverflow.com/a/28162530
 ids.columns = ["Sepal length, cm", "Sepal width, cm",
                "Petal length, cm", "Petal width, cm", "Species"]
 
 # Content of the data (commented out for clarity - too long)
-# print(ids)
+# print(ids) # commented out for clarity only
 
 # head and tail methods print out n first / last respectively lines of data, where n is an argument of the method, default argument is n=5; source: http://www.datasciencemadesimple.com/head-and-tail-in-python-pandas/
 print(ids.head())
@@ -34,13 +34,24 @@ print(ids.tail(3))
 print(ids.describe())
 
 # Histogram of the entire data set, not discriminating the species
-ids.hist()
-pl.show()
-
-# Pandas command `groupby` allows for separating the data set by attribut (of the function) passed in the attribute (of the method); source https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
-print(ids.groupby('Species').size())
+# ids.hist() # commented out for clarity only
+# pl.show() # commented out for clarity only
 
 # Separation of the data per attributes (columns)
 # Confirming the type of the data set
 # firstcol = ids[:, 0] # this command would produce a TypeError. Currently data type is DataFrame (consists of numbers and strings), meaning it cannont be sliced
 print(type(ids))
+
+# Pandas command `groupby` allows for separating the data set by attribut (of the function) passed in the attribute (of the method); source https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
+print(ids.groupby('Species').size())
+
+
+# Split-out validation dataset; source https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
+array = ids.values  # changes datatype to `ndarray`
+# separates the columns with numerical values (first 4 columns); idsv - iris data set values
+idsv = array[:, 0:4]
+print(type(idsv))
+# separates the last column with the species names (50 setosas, 50 versicolors and 50 virginicas)
+spec_names = array[:, 4]
+print(type(spec_names))
+# print(Y)
